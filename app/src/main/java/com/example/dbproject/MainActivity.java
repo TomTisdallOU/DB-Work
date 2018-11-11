6ddc28bf9c4cddb65df7f54d7f6592fc277504af
TomTisdallOU <43801078+TomTisdallOU@users.noreply.github.com>
2018-11-10 22:21:58 -0500

    DB Update

--- app/src/main/java/com/example/dbproject/MainActivity.java
+++ app/src/main/java/com/example/dbproject/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.dbproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addButton = null;
+    Game game;
+    int ID = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDBHandler dbHandler = new MyDBHandler(v.getContext(), null, null, 1);
+                ++ID;
+                Game game = new Game();
+                game.setGameID(ID);
+                game.setHomeTeam("Detroit");
+                game.setAwayTeam("Pittsburgh");
+                Date date = new Date();
+                game.setGameDate(date);
+                dbHandler.addGame_Handler(game);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dbproject/MyDBHandler.java
+++ app/src/main/java/com/example/dbproject/MyDBHandler.java
@@ -6,16 +6,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
-
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.Locale;
 
 
-public class myDBHandler extends SQLiteOpenHelper {
+public class MyDBHandler extends SQLiteOpenHelper {
     //DB Info
     private static final int Database_Version = 1;
     private static final String Database_Name = "FootballPicker";
@@ -27,7 +22,7 @@ public class myDBHandler extends SQLiteOpenHelper {
 
 
 
-    public myDBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public MyDBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, Database_Name, factory, Database_Version);
     }
 
@@ -46,7 +41,7 @@ public class myDBHandler extends SQLiteOpenHelper {
 
     public String loadHandler(){
         String result = "";
-        String query = "Select * From" + Table_Game;
+        String query = "Select * From " + Table_Game;
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor  = db.rawQuery(query, null);
         while (cursor.moveToNext()){
@@ -81,7 +76,15 @@ public class myDBHandler extends SQLiteOpenHelper {
             game.setGameID(cursor.getInt(0));
             game.setHomeTeam(cursor.getString(1));
             game.setAwayTeam(cursor.getString(2));
-            Date date = new SimpleDateFormat("dd/MM/YYYY").parse(cursor.getString(3));
+            String gameDate = cursor.getString(3);
+            Date date = null;
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            try {
+                date = format.parse(gameDate);
+                //System.out.println(date);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             game.setGameDate(date);
             cursor.close();
         } else{
@@ -99,14 +102,23 @@ public class myDBHandler extends SQLiteOpenHelper {
         Game game = new Game();
         if (cursor.moveToFirst()) {
             game.setGameID(cursor.getInt(0));
-            db.delete(Table_Game, )
+            db.delete(Table_Game, Game_Column_ID + " = " , new String[] {String.valueOf(gameID)});
+            cursor.close();
+            result = true;
         }
-
+        db.close();
+        return result;
 
     }
-    public boolean updateHandler(int gameID, String homeTeam){
+    public boolean updateHandler(Game game){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues args = new ContentValues();
+        args.put(Game_Column_ID, game.getGameID());
+        args.put(Game_Column_HomeTeam, game.getHomeTeam());
+        args.put(Game_Column_AwayTeam, game.getAwayTeam());
+        args.put(Game_Column_GameDate, game.getGameDate().toString());
+        return db.update(Table_Game, args, Game_Column_ID + " = " + game.getGameID(), null) > 0;
 
-        
     }
 
 
